f2d70ebdecafdba2c04f079eda9beda6549d9469
Jaydeep Akbari <jaydeepakbari@gmail.com>
2019-08-31 09:53:57 +0530

    Update directory.js

--- directory.js
+++ directory.js
@@ -1,1 +1,25 @@
 
+app.directive('autoComplete', function clientAutoCompleteDir() {
+    return {
+        restrict: 'A', 
+        require: 'ngModel',      
+        link: function (scope, elem, iAttrs, ngModel) {
+            elem.autocomplete({
+                source: function (request, response) {
+                    $.getJSON('api.php&term=" + request.term, function (data) {
+                        response($.map(data, function (value, key) {
+                            return value;
+                        }));
+                    });
+                },
+                minLength: 1,                       
+                select: function (event, ui) {
+                    event.preventDefault();
+                    scope.$apply(function(){
+                        scope[iAttrs.uiParent] = ui.item;
+                    });                       
+                },
+            });
+        }
+    };
+});     
